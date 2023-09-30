2f39b1502a212204776469e2439867226aa0501c
johannpa <johann_paque@hotmail.fr>
2023-09-30 15:19:33 +0200

    Create folder Data for the UserContext

---
+++ Data/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using csharp_crud_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_crud_api.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options): base(options) 
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
