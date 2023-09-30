8628d3eb05d741c7cadb8a9eafe0a1d28c388fb7
johannpa <johann_paque@hotmail.fr>
2023-09-30 15:30:17 +0200

    Create UsersController

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using csharp_crud_api.Data;
+using csharp_crud_api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_crud_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserContext _userContext;
+
+        public UsersController(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        // GET : api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _userContext.Users.ToListAsync();
+        }
+    }
+}
